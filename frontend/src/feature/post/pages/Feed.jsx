483a00b86c6cc0545286776e13fcd57ada90743f
waisshaikh <waisshaikh394@gmail.com>
2026-03-02 16:46:45 +0530

    UI-Layer

---
+++ frontend/src/feature/post/pages/Feed.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './style/feed.scss'
+import Post from '../components/Post'
+
+
+const Feed = () => {
+
+    return (
+        <main className='feed-page'>
+            <div className="feed">
+                <div className="Posts">
+                    {/* <Post/> */}
+
+                
+                </div>
+            </div>
+        </main>
+    )
+}
+
+export default Feed
